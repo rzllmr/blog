c46e786345e42314a04a2413a75fc3c7f4cd1031
rzllmr <30299783+rzllmr@users.noreply.github.com>
2024-05-12 17:07:56 +0200

    Support markdown emojis

--- sw.js
+++ sw.js
@@ -24,9 +24,6 @@ const PRECACHE_LIST = [
   "./img/bg-404.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
-  // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
